af6c2c21e279b50ccbfadf1b7eb58d507685538b
Erwan Le Roux <erwan.le-roux@imt-atlantique.fr>
2026-06-29 14:50:56 +0200

    feature: add datasets

---
+++ datasets/get_dataset_v3.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+def get_dataset_v3(N: int, K: float, u_bar: float, R: float) -> tuple[np.ndarray, np.ndarray]:
+    "Half of the data sample randomly everywhere, half sample centred on the circle"
+    pass
